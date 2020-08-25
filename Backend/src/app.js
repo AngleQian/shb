efa4c851d3022f997a86df5c49dd8f7066d69394
Angle Qian <angleqian01@gmail.com>
2020-08-24 23:41:35 -0400

    Frontend initial setup, minor config changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,24 @@
-import config from './config';
 import express from 'express';
+import config from './config';
 import Logger from './loaders/logger';
 
 
-
 (async function startServer() {
-    const app = express();
+  const app = express();
 
-    await require('./loaders').default({ expressApp: app });
-  
-    app.listen(config.port, err => {
-      if (err) {
-        Logger.error(err);
-        process.exit(1);
-        return;
-      }
-      Logger.info(`
+  await require('./loaders').default({expressApp: app});
+
+  app.listen(config.port, (err) => {
+    if (err) {
+      Logger.error(err);
+      process.exit(1);
+      return;
+    }
+    Logger.info(`
       ################################################
           Server listening on port: ${config.port} 
       ################################################
       `);
-    });
+  });
 })();
 
